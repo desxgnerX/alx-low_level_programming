b05a0bc464e503f580e174a76cdf197565d965e2
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-08 14:00:22 -0800

    C program to output size of primitive data types fix 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	double d;
 	float e;
 
-	printf("Size of a char: %lu \n", (unsigned long) sizeof(b));
-	printf("Size of an int: %lu \n", (unsigned long) sizeof(a));
-	printf("Size of a long int: %lu \n", (unsigned long) sizeof(c));
-	printf("Size of a long long int: %lu \n", sizeof(long long int));
-	printf("Size of a float: %lu \n", (unsigned long) sizeof(e));
+	printf("Size of a char: %lu byte(s) \n", (unsigned long) sizeof(b));
+	printf("Size of an int: %lu byte(s) \n", (unsigned long) sizeof(a));
+	printf("Size of a long int: %lu byte(s) \n", (unsigned long) sizeof(c));
+	printf("Size of a long long int: %lu byte(s) \n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s) \n", (unsigned long) sizeof(e));
 	return (0);
 }
