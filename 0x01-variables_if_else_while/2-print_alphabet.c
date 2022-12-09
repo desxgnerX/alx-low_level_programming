020efc0c26abc2dd842d5bdcc6307b9b18606f20
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-09 15:16:55 -0800

    Task 2 commit 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase letters a-z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
