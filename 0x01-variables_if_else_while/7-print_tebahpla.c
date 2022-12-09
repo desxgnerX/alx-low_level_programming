36f2f37e3cddc5b83bf6a1c9377ba5467501a4db
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-09 15:55:46 -0800

    Task 7 commit 5

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alph[26] = "zyxwvutsrqpomnlkjihgfedcba";
 	int i;
 
-	for (i = 26; i > 1; i--)
+	for (i = 0; i > 26; i++)
 	{
 		putchar(alph[i]);
 	}
