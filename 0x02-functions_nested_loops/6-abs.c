608afddf109fbda7e814e07211c75cd360f7f693
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-13 12:55:21 -0800

    Add find absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _abs - find absolute value of integer
+ *  @a: the number
+ *
+ *  Return: absolute value
+ */
+
+int _abs(int a)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
