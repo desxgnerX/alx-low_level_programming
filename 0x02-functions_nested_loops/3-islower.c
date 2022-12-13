ca3b0f21d8f415d00ba2da1d089fee4a9a5bdf68
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-13 12:29:37 -0800

    Add script to check if character is lowercase fix 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,15 +4,13 @@
  * _islower - checks if input is lower case
  * @c: the character
  *
- * Return: 1 if lowercase and 0 if not
+ * Return: 1 if character is lowercase, 0 if not
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (0);
-	}
 	else
 		return (1);
 }
