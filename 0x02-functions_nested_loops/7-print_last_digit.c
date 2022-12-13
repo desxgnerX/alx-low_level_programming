a3969f20b85b180719bc3577f36fdd6ffd884e2e
desxgnerX <117768170+desxgnerX@users.noreply.github.com>
2022-12-13 23:12:41 +0200

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_last_digit - print last digit of number
+ * @n: the number
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int)
+{
+    int i;
+  
+    i = n % 10;
+    return (i);
+}
