6380840164fbc3c4deed25a2ddad2842af81e265
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-15 09:15:17 -0800

    Task 9 fix 1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,18 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
-			printf("Fizz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
-		else if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("FizzBuzz ");
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 15 == 0)
+			printf("FizzBuzz");
 		else
 			printf("%d ", i);
+		if (i < 100)
+			printf(" ");
 	}
+	printf("\n");
+	
+	return (0);
 }
