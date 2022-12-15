7407b9346452bb43a4ef62be0f2a00679ef16133
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-15 09:10:17 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - FizzBuzz test
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+}
