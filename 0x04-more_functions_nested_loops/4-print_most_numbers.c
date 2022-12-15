d0124a893e31a5ed1ffc33ba6605ead4aa10eabe
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-15 07:48:24 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers excluding 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != 2 && i != 4)
+			continue;
+		else
+			_putchar(i);
+	}
+}
