ec4ddb23b8a346652eae3796397e6cad1aacb314
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-19 13:33:47 -0800

    Task 4 fix 2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,18 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
-	while (s[i++])
+	while (*s != '\0')
+	{
 		length++;
+		i++;
+	}
+	s--;
 
-	for (i = length - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (i = lenght; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
 
 	_putchar('\n');
 }
