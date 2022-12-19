de0b36a1bb10b316f642ad83e4718d2c09b87b6d
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-19 14:22:07 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * puts2 - prints every other character starting with the first
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
