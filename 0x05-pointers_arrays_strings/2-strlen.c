eaaa93a98640ab0e4ee905a502c7537b5edf0343
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-19 12:34:49 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - prints length of string
+ * @s: the string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+		length++;
+
+	return (length);
+}
