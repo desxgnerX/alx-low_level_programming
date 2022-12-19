25cceb8c3ea7b6ffe188369a82ad656e6da0fcdf
desxgnerX <117768170+desxgnerX@users.noreply.github.com>
2022-12-20 01:45:24 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,13 @@
 
 void print_rev(char *s)
 {
-	int swap;
-	int i = 0;
-	while (s[i])
-		i++;
-	while (i--)
-		_putchar(s[i]);
-	
-	_putchar('/n');
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
 }
