15451ee654d4a055532913a297b5382b87c8fd05
Tanaka Sipindiye <alexander.sipindiye@gmail.com>
2022-12-22 07:05:09 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
